10b5a5d9e4bd143f8bfeb990b86b5c6dbf2b0596
mashkovd <mashkovd@live.ru>
2026-03-28 20:58:59 +0100

    fix: show footer only on home page
    Footer was appearing on all pages including guides and subpages.

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -3,14 +3,19 @@
     <template #nav-bar-title-before>
       <BrandMark />
     </template>
-    <template #layout-bottom>
+    <template v-if="isHome" #layout-bottom>
       <DocsFooter />
     </template>
   </DefaultTheme.Layout>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import BrandMark from './components/BrandMark.vue'
 import DocsFooter from './components/DocsFooter.vue'
+
+const route = useRoute()
+const isHome = computed(() => route.path === '/' || route.path === '/index.html')
 </script>
